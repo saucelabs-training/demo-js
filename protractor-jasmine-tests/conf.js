f465306a8cfe1bc159721894bd63b90784a8d7b7
James Tacker <james.tacker@saucelabs.com>
2019-06-05 22:45:03 -0700

    updated conf.js

--- protractor-jasmine-tests/conf.js
+++ protractor-jasmine-tests/conf.js
@@ -1,62 +1,47 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 exports.config = {
-    /*sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,*/
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
     sauceRegion: 'us',
     seleniumAddress: 'https://ondemand.saucelabs.com/wd/hub',
     specs: ['specs/*spec.js'],
 
-    multiCapabilities: [{
-        browserName: 'chrome',
-        version: 'latest',
-        platform: 'Windows 10',
-        name: 'chrome-protractor-test',
-        build: 'Sample Protractor Tests',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        shardTestFiles: true,
-        maxInstances: 25
-    },{
-        browserName: 'firefox',
-        version: 'latest',
-        platform: 'Windows 10',
-        name: 'firefox-protractor-test',
-        build: 'Sample Protractor Tests',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        shardTestFiles: true,
-        maxInstances: 25
-    }],
-
-    /**
-     * The below version should work for the upcoming Selenium 4 release and w3c protocol,
-     * although currently the protractor webdriver manager can't resolve 'sauce:options'
-     --------------------------------------------------------------------------------
-
-     browserName: 'chrome',
-     browserVersion: 'latest',
-     platformVersion: 'Windows 10',
-     'sauce:options': {
-            username: process.env.SAUCE_USERNAME,
-            accessKey: process.env.SAUCE_ACCESS_KEY,
-            name: 'chrome-protractor-test',
-            build: 'Sample Protractor Tests',
-            seleniumVersion: '3.141.59'
-        },*/
-
-    baseUrl: 'https://www.saucedemo.com',
-
     onPrepare: async () => {
-        await browser.waitForAngularEnabled(false);
         const caps = await browser.getCapabilities();
         jasmine.getEnv().addReporter(new SpecReporter({
             spec: {
                 displayStacktrace: true,
             }
         }));
-    },
+        },
+        framework: 'jasmine',
+        multiCapabilities: [{
+            browserName: 'chrome',
+            browserVersion: 'latest',
+            platformName: 'Windows 10',
+            chromeOptions : { 'w3c' : true },
+            'sauce:options': {
+                seleniumVersion: '3.141.59',
+                name: 'chrome-protractor-test',
+                build: 'Sample Protractor Tests'
+            },
+            shardTestFiles: true,
+            maxInstances: 25
+        },{
+            browserName: 'firefox',
+            browserVersion: 'latest',
+            platformName: 'Windows 10',
+            'sauce:options': {
+                seleniumVersion: '3.141.59',
+                name: 'chrome-protractor-test',
+                build: 'Sample Protractor Tests'
+            },
+            shardTestFiles: true,
+            maxInstances: 25
+    }],
 
-    framework: 'jasmine',
+    baseUrl: 'https://www.saucedemo.com',
+
     SELENIUM_PROMISE_MANAGER: false,
 
     onComplete: async () => {
