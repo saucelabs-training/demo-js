eb7e70b6f415c04d266dfb910537b8a72e820ea2
James Tacker <45496368+spider-sauce@users.noreply.github.com>
2019-06-24 08:50:34 -0700

    updated conf.js (#5)
    * updated conf.js
    
    * updated package.json and conf.js
    
    * Fix protractor selenium 4 example
    
    * remove Firefox keys

--- protractor-jasmine-tests/conf.js
+++ protractor-jasmine-tests/conf.js
@@ -1,70 +1,55 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 exports.config = {
-    /*sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,*/
-    sauceRegion: 'us',
-    seleniumAddress: 'https://ondemand.saucelabs.com/wd/hub',
-    specs: ['specs/*spec.js'],
+	sauceUser: process.env.SAUCE_USERNAME,
+	sauceKey: process.env.SAUCE_ACCESS_KEY,
+	sauceRegion: 'us',
+	specs: [ 'specs/*spec.js' ],
 
-    multiCapabilities: [{
-        browserName: 'chrome',
-        version: 'latest',
-        platform: 'Windows 10',
-        name: 'chrome-protractor-test',
-        build: 'Sample Protractor Tests',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        shardTestFiles: true,
-        maxInstances: 25
-    },{
-        browserName: 'firefox',
-        version: 'latest',
-        platform: 'Windows 10',
-        name: 'firefox-protractor-test',
-        build: 'Sample Protractor Tests',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        shardTestFiles: true,
-        maxInstances: 25
-    }],
+	onPrepare: async () => {
+		await browser.waitForAngularEnabled(false);
+		const caps = await browser.getCapabilities();
+		jasmine.getEnv().addReporter(new SpecReporter({
+			spec: {
+				displayStacktrace: true,
+			}
+		}));
+	},
+	framework: 'jasmine',
+	multiCapabilities: [
+		{
+			browserName: 'chrome',
+			browserVersion: 'latest',
+			platformName: 'Windows 10',
+			'goog:chromeOptions': { 'w3c': true },
+			'sauce:options': {
+				seleniumVersion: '3.141.59',
+				name: 'chrome-protractor-test',
+				build: 'Sample Protractor Tests'
+			},
+			shardTestFiles: true,
+			maxInstances: 25
+		},
+		{
+			browserName: 'firefox',
+			browserVersion: 'latest',
+			platformName: 'Windows 10',
+			'sauce:options': {
+				seleniumVersion: '3.141.59',
+				name: 'firefox-protractor-test',
+				build: 'Sample Protractor Tests'
+			},
+			shardTestFiles: true,
+			maxInstances: 25
+		},
+	],
+	baseUrl: 'https://www.saucedemo.com',
 
-    /**
-     * The below version should work for the upcoming Selenium 4 release and w3c protocol,
-     * although currently the protractor webdriver manager can't resolve 'sauce:options'
-     --------------------------------------------------------------------------------
-
-     browserName: 'chrome',
-     browserVersion: 'latest',
-     platformVersion: 'Windows 10',
-     'sauce:options': {
-            username: process.env.SAUCE_USERNAME,
-            accessKey: process.env.SAUCE_ACCESS_KEY,
-            name: 'chrome-protractor-test',
-            build: 'Sample Protractor Tests',
-            seleniumVersion: '3.141.59'
-        },*/
-
-    baseUrl: 'https://www.saucedemo.com',
-
-    onPrepare: async () => {
-        await browser.waitForAngularEnabled(false);
-        const caps = await browser.getCapabilities();
-        jasmine.getEnv().addReporter(new SpecReporter({
-            spec: {
-                displayStacktrace: true,
-            }
-        }));
-    },
-
-    framework: 'jasmine',
-    SELENIUM_PROMISE_MANAGER: false,
-
-    onComplete: async () => {
-        let printSessionId = async (jobName) => {
-            let session = await browser.getSession();
-            console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-            };
-        printSessionId("Insert Job Name Here");
-        //await browser.executeScript("sauce:job-result=" + (SpecReporter.valueOf().result());
-    },
+	onComplete: async () => {
+		let printSessionId = async (jobName) => {
+			let session = await browser.getSession();
+			console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+		};
+		printSessionId('Insert Job Name Here');
+		//await browser.executeScript("sauce:job-result=" + (SpecReporter.valueOf().result());
+	},
 };
