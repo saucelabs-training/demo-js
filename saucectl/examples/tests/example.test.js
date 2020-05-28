32e0f1beb805016e1610bb1c8b43bdf08c82d467
nadvolod <nadvolod@gmail.com>
2020-05-28 16:56:09 -0400

    add saucectl

---
+++ saucectl/examples/tests/example.test.js
@@ -0,0 +1,15 @@
+describe('SauceDemo login page', () => {
+	test('should load', async () => {
+		const page = (await browser.pages())[0]
+		await page.goto('https://www.saucedemo.com/');
+		expect(await page.title()).toBe('Swag Labs');
+	});
+	test('should login', async () => {
+		const page = (await browser.pages())[0]
+		await page.goto('https://www.saucedemo.com/');
+		await page.type('#user-name', 'standard_user');
+		await page.type('#password', 'standard_user');
+		await page.click('.btn_action');
+		expect(await page.title()).toBe('Swag Labs');
+	});
+});
