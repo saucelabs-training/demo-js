464560d2134f56aa0820f9e8096ea7eca625f394
nadvolod <nadvolod@gmail.com>
2020-06-12 12:22:49 -0400

    add readme and rename foldeer

--- testrunner-toolkit/examples/tests/example.test.js
+++ testrunner-toolkit/examples/tests/example.test.js
@@ -8,7 +8,7 @@ describe('SauceDemo login page', () => {
 		const page = (await browser.pages())[0]
 		await page.goto('https://www.saucedemo.com/');
 		await page.type('#user-name', 'standard_user');
-		await page.type('#password', 'standard_user');
+		await page.type('#password', 'secret_sauce');
 		await page.click('.btn_action');
 		expect(await page.title()).toBe('Swag Labs');
 	});
