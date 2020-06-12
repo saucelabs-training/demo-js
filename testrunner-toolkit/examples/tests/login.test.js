5fa24da01abe70c63728f284d53db9aff9381337
nadvolod <nadvolod@gmail.com>
2020-06-12 12:33:42 -0400

    update api calls

--- testrunner-toolkit/examples/tests/login.test.js
+++ testrunner-toolkit/examples/tests/login.test.js
@@ -1,15 +1,17 @@
 describe('SauceDemo login page', () => {
 	test('should load', async () => {
-		const page = (await browser.pages())[0]
+		const page = await browser.newPage();
 		await page.goto('https://www.saucedemo.com/');
 		expect(await page.title()).toBe('Swag Labs');
+		await browser.close();
 	});
 	test('should login', async () => {
-		const page = (await browser.pages())[0]
+		const page = await browser.newPage();
 		await page.goto('https://www.saucedemo.com/');
 		await page.type('#user-name', 'standard_user');
 		await page.type('#password', 'secret_sauce');
 		await page.click('.btn_action');
 		expect(await page.title()).toBe('Swag Labs');
+		await browser.close();
 	});
 });
