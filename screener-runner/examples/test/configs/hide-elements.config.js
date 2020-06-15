4293e7788d80fd0b4a58680c803e87a568a92a75
nadvolod <nadvolod@gmail.com>
2020-06-15 10:09:13 -0400

    clean up dead code

--- screener-runner/examples/test/configs/hide-elements.config.js
+++ screener-runner/examples/test/configs/hide-elements.config.js
@@ -17,21 +17,8 @@ module.exports = {
             url: 'https://www.saucedemo.com',
             name: 'Hide element example',
         },
-        //If you wanted to dynamically preload the page, you'd do it like this
-        // {
-        //   //This page has dynamic elements that only appear when they are scrolled into view
-        //   //so we run a script to preload those elements
-        //   url: 'https://www.ultimateqa.com',
-        //   name: 'Ultimate QA Home Page',
-        //   steps: new Steps()
-        //   .executeScript('window.scrollTo(0,document.body.scrollHeight)')
-        //   .wait(4000)
-        //   .snapshot('Loaded')
-        //   .end()
-        // }
     ],
     //What are all of the browsers that we want to test against
-    //In this case, they are all overriden in the screener.sauce.config
     browsers: [
         {
             browserName: 'chrome'
