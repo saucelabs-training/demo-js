37dab0a5e0262c86e2e001cdf02f02ff1114ab59
Wim Selles <wim@wswebcreation.nl>
2020-07-15 07:54:50 +0200

    chore: remove sample playwright test

--- testrunner-toolkit/playwright/tests/example.test.js
+++
@@ -1,6 +0,0 @@
-describe('saucectl demo test', () => {
-	test('should verify title of the page', async () => {
-		await page.goto('https://www.saucedemo.com/');
-		expect(await page.title()).toBe('Swag Labs');
-	});
-});
