0f5c8e799e40c590f945869197820b4f3184f8f6
Wim Selles <wim@wswebcreation.nl>
2020-09-06 16:35:02 +0200

    chore: update dependencies for wdio-6

--- webdriverio/web/framework/test/configs/wdio.shared.conf.js
+++ webdriverio/web/framework/test/configs/wdio.shared.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // ===================
     // Test Configurations
     // ===================
-    logLevel: 'debug',
+    logLevel: 'silent',
     bail: 0,
     baseUrl: 'https://www.saucedemo.com/',
     waitforTimeout: 10000,
