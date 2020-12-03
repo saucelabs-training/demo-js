ecdf286f79d895459ca4dae41e7a44260bbc9262
Wim Selles <wim@wswebcreation.nl>
2020-12-03 13:04:21 +0100

    chore: update docs, push script and apps

--- webdriverio/mobile-app/examples/gestures/scripts/push_apps_to_storage.sh
+++ webdriverio/mobile-app/examples/gestures/scripts/push_apps_to_storage.sh
@@ -3,18 +3,18 @@
 ## US
 # Android
 curl \
-  -F "payload=@../apps/Android.SauceLabs.Mobile.Sample.app.apk" \
+  -F "payload=@../test-apps/Android.SauceLabs.Mobile.Sample.app.apk" \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.zip" \
+  -F "payload=@../test-apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.zip" \
   -F name=sample-app-ios.zip \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 curl \
-  -F "payload=@../apps/iOS.RealDevice.SauceLabs.Mobile.Sample.app.ipa" \
+  -F "payload=@../test-apps/iOS.RealDevice.SauceLabs.Mobile.Sample.app.ipa" \
   -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
@@ -22,18 +22,18 @@ curl \
 ## EU
 # Android
 curl \
-  -F "payload=@../apps/Android.SauceLabs.Mobile.Sample.app.apk" \
+  -F "payload=@../test-apps/Android.SauceLabs.Mobile.Sample.app.apk" \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.zip" \
+  -F "payload=@../test-apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.zip" \
   -F name=sample-app-ios.zip \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 curl \
-  -F "payload=@../apps/iOS.RealDevice.SauceLabs.Mobile.Sample.app.ipa" \
+  -F "payload=@../test-apps/iOS.RealDevice.SauceLabs.Mobile.Sample.app.ipa" \
   -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
