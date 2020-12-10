face43d1e942c73dac45a0708b8be9c7c28d3623
nadvolod <nadvolod@gmail.com>
2020-12-10 09:31:27 -0500

    add another test

--- visual-e2e/examples/test/configs/wdio.visual.conf.js
+++ visual-e2e/examples/test/configs/wdio.visual.conf.js
@@ -1,3 +1,13 @@
+const sauceOptions = {
+    username: process.env.SAUCE_USERNAME,
+    accesskey: process.env.SAUCE_ACCESS_KEY
+};
+const visualOptions = {
+    apiKey: process.env.SCREENER_API_KEY,
+    projectName: 'UltimateQA Visual',
+    scrollAndStitchScreenshots: true
+};
+
 exports.config = {
     // ====================
     // More information about the configuration is found here https://webdriver.io/docs/configurationfile.html
@@ -22,6 +32,11 @@ exports.config = {
     protocol: 'https',
     path: '/wd/hub',
     region: process.env.REGION || 'us',
+    //Desktop A 28%: https://www.w3schools.com/browsers/browsers_display.asp
+    //Desktop B 20%: https://www.w3schools.com/browsers/browsers_display.asp
+    //iphone X
+    //iphone 6-8
+    //Galaxy S8+
     capabilities: ['1280x1024',
         '1920x1080',
         '375x812',
@@ -32,12 +47,10 @@ exports.config = {
             platformName: 'macOS 10.15',
             browserVersion: 'latest',
             'sauce:options': {
-                username: process.env.SAUCE_USERNAME,
-                accesskey: process.env.SAUCE_ACCESS_KEY
+                ...sauceOptions,
             },
             'sauce:visual': {
-                apiKey: process.env.SCREENER_API_KEY,
-                projectName: 'demo-js',
+                ...visualOptions,
                 viewportSize: `${viewportSize}`
             }
         }
--- visual-e2e/examples/test/specs/visual.crossplatform.spec.js
+++ visual-e2e/examples/test/specs/visual.crossplatform.spec.js
@@ -1,8 +1,13 @@
-describe('Home page', () => {
-    it('should look correct visually', () => {
+describe('Visual appearance', () => {
+    it('should look correct for home page', () => {
         browser.url('');
         browser.execute('/*@visual.init*/', 'Sauce Demo App');
         browser.execute('/*@visual.snapshot*/', 'Home Page');
+    });
+    it('should look correct visually', () => {
+        browser.url('/inventory.html');
+        browser.execute('/*@visual.init*/', 'Sauce Demo App');
+        browser.execute('/*@visual.snapshot*/', 'Inventory Page');
     });
 });
 
