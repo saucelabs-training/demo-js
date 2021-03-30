44b31c9f82ea5abc2e809de84eba7ec5fc1b69bc
nadvolod <nadvolod@gmail.com>
2021-03-30 09:27:34 -0400

    Update webdriverio/webdriver/examples/typescript/test/pageobjects/page.ts
    Co-authored-by: Wim Selles <wim@wswebcreation.nl>

--- webdriverio/webdriver/examples/typescript/test/pageobjects/page.ts
+++ webdriverio/webdriver/examples/typescript/test/pageobjects/page.ts
@@ -7,7 +7,7 @@ export default class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path: string) {
+    open (path: string):void {
         browser.url(`https://the-internet.herokuapp.com/${path}`)
     }
 }
