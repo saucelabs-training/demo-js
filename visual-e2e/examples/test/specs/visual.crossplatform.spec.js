b9b8751eda31dae29cf5ffe82271343ab8f1a2bb
nadvolod <nadvolod@gmail.com>
2021-08-23 13:10:36 -0400

    add async

--- visual-e2e/examples/test/specs/visual.crossplatform.spec.js
+++ visual-e2e/examples/test/specs/visual.crossplatform.spec.js
@@ -1,13 +1,13 @@
 describe('Visual appearance', () => {
-    it('should look correct', () => {
-        browser.url('');
-        browser.execute('/*@visual.init*/', 'My React App');
-        browser.execute('/*@visual.snapshot*/', 'Home Page');
+    it('should look correct', async () => {
+        await browser.url('');
+        await browser.execute('/*@visual.init*/', 'My React App');
+        await browser.execute('/*@visual.snapshot*/', 'Home Page');
 
-        browser.url('/inventory.html');
-        browser.execute('/*@visual.snapshot*/', 'Inventory Page');
+        await browser.url('/inventory.html');
+        await browser.execute('/*@visual.snapshot*/', 'Inventory Page');
 
-        const result = browser.execute('/*@visual.end*/');
+        const result = await browser.execute('/*@visual.end*/');
         expect(result.message).toBeNull();
     });
 });
