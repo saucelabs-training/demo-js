81aa9195e576a73731f2646c961302298188f12e
Wim Selles <wim@wswebcreation.nl>
2021-09-01 14:12:59 +0200

    chore: update WDIO perfomance
    - update deps
    - transform to sync

--- webdriverio/appium-web/examples/device-allocation/test/configs/wdio.saucelabs.conf.js
+++ webdriverio/appium-web/examples/device-allocation/test/configs/wdio.saucelabs.conf.js
@@ -1,5 +1,5 @@
 const {config} = require('./wdio.shared.conf');
-const build= `WebdriverIO RDC UP build-${new Date().getTime()}`;
+const build = `WebdriverIO RDC UP build-${new Date().getTime()}`;
 
 // =========================
 // Sauce RDC specific config
@@ -18,186 +18,186 @@ config.region = process.env.REGION || 'us';
 // https://wiki.saucelabs.com/display/DOCS/Appium+Capabilities+for+Real+Device+Testing
 // ===================================================================================
 config.capabilities = [
-    /**
-     * Capabilities to run on a specific device based on the deviceID
-     */
-    {
-        name: 'Run on deviceID iOS',
-        browserName: 'safari',
-        // When you have private devices in EU and US
-        deviceName: process.env.REGION === 'eu' ? 'iPhone_XS_ws' : 'iPhone_SE_2020_POC142',
-        platformName: 'iOS',
-        // Extra caps
-        cacheId: '17506a5f122', // See the capabilities url as mentioned above
-       build
-    },
-    {
-        name: "Run on deviceID Android",
-        browserName: 'chrome',
-        // When you have private devices in EU and US
-        deviceName: process.env.REGION === 'eu' ? 'Samsung_Galaxy_S10_ws' : 'Google_Pixel_3_XL_ws_us',
-        platformName: 'Android',
-        // Extra caps
-        cacheId: '58vi9zm82i', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a specific device based on its name
-     */
-    {
-        name: 'Run on device description for iOS',
-        browserName: 'safari',
-        deviceName: 'iPhone XS',
-        platformName: 'iOS',
-        // Extra caps
-        cacheId: 'e7s54tyw5g', // See the capabilities url as mentioned above
-       build
-    },
-    {
-        name: "Run on device description for Android",
-        browserName: 'chrome',
-        deviceName: 'Samsung Galaxy S10',
-        platformName: 'Android',
-        // Extra caps
-        cacheId: 'tl5lps3go1', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a 'random' device based on a regular expression
-     * This will give a device based on:
-     * - matching regular expression
-     * - direct availability
-     */
-    {
-        name: 'Run on deviceName regex iOS - iPhone [678]?.*',
-        browserName: 'safari',
-        deviceName: 'iPhone [678]?.*',
-        platformName: 'iOS',
-        // Extra caps
-        cacheId: 'vvqb5g7lr3', // See the capabilities url as mentioned above
-        // Specs are not mentioned here so it will run all tests
-        // from ./test/specs/
-        build
-    },
-    {
-        name: "Run on deviceName regex Android - Samsung Galaxy S?.*",
-        browserName: 'chrome',
-        deviceName: 'Samsung Galaxy S?.*',
-        platformName: 'Android',
-        // Extra caps
-        cacheId: '4hn1znudgm', // See the capabilities url as mentioned above
-        // Specs are not mentioned here so it will run all tests
-        // from ./test/specs/
-        build
-    },
-    /**
-     * Capabilities to run on a major version of the OS
-     */
-    {
-        name: 'Run on major iOS version - 13',
-        browserName: 'safari',
-        platformName: 'iOS',
-        platformVersion: '13',
-        // Extra caps
-        cacheId: 'bq9jvoctq7', // See the capabilities url as mentioned above
-       build
-    },
-    {
-        name: 'Run on major Android version - 7',
-        browserName: 'chrome',
-        platformName: 'Android',
-        platformVersion: '7',
-        // Extra caps
-        cacheId: 'cvtjmvawq8', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a minor version of the OS
-     */
-    {
-        name: 'Run on minor iOS version - 12.1',
-        browserName: 'safari',
-        platformName: 'iOS',
-        platformVersion: '12.1',
-        // Extra caps
-        cacheId: 'em1pf4ab20', // See the capabilities url as mentioned above
-       build
-    },
-    {
-        name: 'Run on minor Android version - 7.1',
-        browserName: 'chrome',
-        platformName: 'Android',
-        platformVersion: '7.1',
-        // Extra caps
-        cacheId: 'cegc4zom7n', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a patch version of the OS
-     */
-    {
-        name: 'Run on patch iOS version - 14.4.1',
-        browserName: 'safari',
-        platformName: 'iOS',
-        platformVersion: '14.4.1',
-        // Extra caps
-        cacheId: 'dn7zr1irmc', // See the capabilities url as mentioned above
-       build
-    },
-    {
-        name: 'Run on patch Android version - 6.0.1',
-        browserName: 'chrome',
-        platformName: 'Android',
-        platformVersion: '6.0.1',
-        // Extra caps
-        cacheId: 'itfdqgn5mp', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a phone only (works for iOS and Android the same)
-     */
-    {
-        name: 'Run iOS phone only',
-        browserName: 'safari',
-        platformName: 'iOS',
-        phoneOnly: true,
-        // Extra caps
-        cacheId: 'q96a2zipwp', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on a tablet only (works for iOS and Android the same)
-     */
-    {
-        name: 'Run Android tablet only',
-        browserName: 'chrome',
-        platformName: 'Android',
-        tabletOnly: true,
-        // Extra caps
-        cacheId: 'nn7g8kq8m8', // See the capabilities url as mentioned above
-       build
-    },
-    /**
-     * Capabilities to run on private devices only
-     */
-    {
-        name: 'Run iOS private devices only',
-        browserName: 'safari',
-        platformName: 'iOS',
-        privateDevicesOnly: true,
-        // Extra caps
-        cacheId: 'zr6g2phl4w', // See the capabilities url as mentioned above
-        build
-    },
-    {
-        name: 'Run Android private devices only',
-        browserName: 'chrome',
-        platformName: 'Android',
-        privateDevicesOnly: true,
-        // Extra caps
-        cacheId: 'edvxh39cnd', // See the capabilities url as mentioned above
-       build
-    },
+  /**
+   * Capabilities to run on a specific device based on the deviceID
+   */
+  {
+    name: 'Run on deviceID iOS',
+    browserName: 'safari',
+    // When you have private devices in EU and US
+    deviceName: process.env.REGION === 'eu' ? 'iPhone_XS_ws' : 'iPhone_SE_2020_POC142',
+    platformName: 'iOS',
+    // Extra caps
+    cacheId: '17506a5f122', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: "Run on deviceID Android",
+    browserName: 'chrome',
+    // When you have private devices in EU and US
+    deviceName: process.env.REGION === 'eu' ? 'Samsung_Galaxy_S10_ws' : 'Google_Pixel_3_XL_ws_us',
+    platformName: 'Android',
+    // Extra caps
+    cacheId: '58vi9zm82i', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a specific device based on its name
+   */
+  {
+    name: 'Run on device description for iOS',
+    browserName: 'safari',
+    deviceName: 'iPhone XS',
+    platformName: 'iOS',
+    // Extra caps
+    cacheId: 'e7s54tyw5g', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: "Run on device description for Android",
+    browserName: 'chrome',
+    deviceName: 'Samsung Galaxy S10',
+    platformName: 'Android',
+    // Extra caps
+    cacheId: 'tl5lps3go1', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a 'random' device based on a regular expression
+   * This will give a device based on:
+   * - matching regular expression
+   * - direct availability
+   */
+  {
+    name: 'Run on deviceName regex iOS - iPhone [678]?.*',
+    browserName: 'safari',
+    deviceName: 'iPhone [678]?.*',
+    platformName: 'iOS',
+    // Extra caps
+    cacheId: 'vvqb5g7lr3', // See the capabilities url as mentioned above
+    // Specs are not mentioned here so it will run all tests
+    // from ./test/specs/
+    build
+  },
+  {
+    name: "Run on deviceName regex Android - Samsung Galaxy S?.*",
+    browserName: 'chrome',
+    deviceName: 'Samsung Galaxy S?.*',
+    platformName: 'Android',
+    // Extra caps
+    cacheId: '4hn1znudgm', // See the capabilities url as mentioned above
+    // Specs are not mentioned here so it will run all tests
+    // from ./test/specs/
+    build
+  },
+  /**
+   * Capabilities to run on a major version of the OS
+   */
+  {
+    name: 'Run on major iOS version - 13',
+    browserName: 'safari',
+    platformName: 'iOS',
+    platformVersion: '13',
+    // Extra caps
+    cacheId: 'bq9jvoctq7', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: 'Run on major Android version - 7',
+    browserName: 'chrome',
+    platformName: 'Android',
+    platformVersion: '7',
+    // Extra caps
+    cacheId: 'cvtjmvawq8', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a minor version of the OS
+   */
+  {
+    name: 'Run on minor iOS version - 12.1',
+    browserName: 'safari',
+    platformName: 'iOS',
+    platformVersion: '12.1',
+    // Extra caps
+    cacheId: 'em1pf4ab20', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: 'Run on minor Android version - 7.1',
+    browserName: 'chrome',
+    platformName: 'Android',
+    platformVersion: '7.1',
+    // Extra caps
+    cacheId: 'cegc4zom7n', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a patch version of the OS
+   */
+  {
+    name: 'Run on patch iOS version - 14.4.1',
+    browserName: 'safari',
+    platformName: 'iOS',
+    platformVersion: '14.4.1',
+    // Extra caps
+    cacheId: 'dn7zr1irmc', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: 'Run on patch Android version - 6.0.1',
+    browserName: 'chrome',
+    platformName: 'Android',
+    platformVersion: '6.0.1',
+    // Extra caps
+    cacheId: 'itfdqgn5mp', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a phone only (works for iOS and Android the same)
+   */
+  {
+    name: 'Run iOS phone only',
+    browserName: 'safari',
+    platformName: 'iOS',
+    phoneOnly: true,
+    // Extra caps
+    cacheId: 'q96a2zipwp', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on a tablet only (works for iOS and Android the same)
+   */
+  {
+    name: 'Run Android tablet only',
+    browserName: 'chrome',
+    platformName: 'Android',
+    tabletOnly: true,
+    // Extra caps
+    cacheId: 'nn7g8kq8m8', // See the capabilities url as mentioned above
+    build
+  },
+  /**
+   * Capabilities to run on private devices only
+   */
+  {
+    name: 'Run iOS private devices only',
+    browserName: 'safari',
+    platformName: 'iOS',
+    privateDevicesOnly: true,
+    // Extra caps
+    cacheId: 'zr6g2phl4w', // See the capabilities url as mentioned above
+    build
+  },
+  {
+    name: 'Run Android private devices only',
+    browserName: 'chrome',
+    platformName: 'Android',
+    privateDevicesOnly: true,
+    // Extra caps
+    cacheId: 'edvxh39cnd', // See the capabilities url as mentioned above
+    build
+  },
 ];
 
 config.services = config.services.concat('sauce');
--- webdriverio/webdriver/examples/performance/test/specs/front.end.performance.spec.js
+++ webdriverio/webdriver/examples/performance/test/specs/front.end.performance.spec.js
@@ -1,24 +1,24 @@
 describe('Sauce Labs Front-End Performance', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     //
     // Adding extra logs to the Sauce Commands Dashboard
-    browser.execute('sauce:context=########## Start beforeEach ##########');
+    await browser.execute('sauce:context=########## Start beforeEach ##########');
     //
     // Now load the url and wait for it to be displayed
-    browser.url('');
+    await browser.url('');
 
     //
     // Adding extra logs to the Sauce Commands Dashboard
-    browser.execute('sauce:context=########## End beforeEach ##########');
+    await browser.execute('sauce:context=########## End beforeEach ##########');
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     //
     // Adding extra logs to the Sauce Commands Dashboard
-    browser.execute('sauce:context=########## Enf of test ##########');
+    await browser.execute('sauce:context=########## Enf of test ##########');
   });
 
-  it('logs (sauce:performance) should check if all metrics were captured', () => {
+  it('logs (sauce:performance) should check if all metrics were captured', async () => {
     //
     // The expected metrics
     const metrics = [
@@ -42,34 +42,34 @@ describe('Sauce Labs Front-End Performance', () => {
     ];
     //
     // Get the performance logs
-    const performance = browser.execute('sauce:log', {type: 'sauce:performance'});
+    const performance = await browser.execute('sauce:log', {type: 'sauce:performance'});
 
     //
     // Verify that all logs have been captured
     metrics.forEach(metric => expect(metric in performance, `${metric} metric is missing`));
   });
 
-  it('(sauce:performance) should validate speedIndex', () => {
+  it('(sauce:performance) should validate speedIndex', async () => {
     //
     // Get the performance logs
-    const performance = browser.execute('sauce:log', {type: 'sauce:performance'});
+    const performance = await browser.execute('sauce:log', {type: 'sauce:performance'});
 
     //
     // Verify that all logs have been captured
     expect(performance.speedIndex < 1000, `${performance.speedIndex} is equal or bigger than 100`);
   });
 
-  it('(sauce:jankinessCheck) should validate the smoothness of the page', () => {
+  it('(sauce:jankinessCheck) should validate the smoothness of the page', async () => {
     //
     // Login
-    $('[data-test="username"]').setValue('standard_user');
-    $('[data-test="password"]').setValue('secret_sauce');
-    $('#login-button').click();
-    $('#inventory_container').waitForDisplayed({timeout: 15000});
+    await $('[data-test="username"]').setValue('standard_user');
+    await $('[data-test="password"]').setValue('secret_sauce');
+    await $('#login-button').click();
+    await $('#inventory_container').waitForDisplayed({timeout: 15000});
 
     //
     // Get the jankiness results
-    const jankiness = browser.execute('sauce:jankinessCheck');
+    const jankiness = await browser.execute('sauce:jankinessCheck');
 
     // Verify that all logs have been captured
     expect(jankiness.score > 0.7, `${jankiness.score} is equal or lower than 0.7`);
