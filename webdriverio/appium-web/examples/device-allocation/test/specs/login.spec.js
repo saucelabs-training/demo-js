593562d94086a4143e5a6d630d2cea3e25f1e088
Wim Selles <wim@wswebcreation.nl>
2022-06-20 07:38:37 +0200

    chore: update deps and simplify allocation spec

--- webdriverio/appium-web/examples/device-allocation/test/specs/login.spec.js
+++ webdriverio/appium-web/examples/device-allocation/test/specs/login.spec.js
@@ -1,19 +1,6 @@
 describe('LoginPage', () => {
-    beforeEach(async () => {
-        await browser.url('');
-        await $('#login_button_container').waitForDisplayed();
-    });
-
-    it('should be able to login with a standard user', async () => {
-        await $('#user-name').addValue('standard_user');
-        await $('#password').addValue('secret_sauce');
-        // For some reason Android is not clicking properly
-        driver.isAndroid
-          ? await browser.execute('document.querySelector(\'.btn_action\').click()')
-          : await $('.btn_action').click();
-
-        // We will not execute an assertion here because if the page is not displayed it will
-        // already throw an error
-        await $('.inventory_list').waitForDisplayed();
-    });
+  it('should be able determine that the page loaded', async () => {
+    await browser.url('');
+    await $('#login_button_container').waitForDisplayed();
+  });
 });
