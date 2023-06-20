7ce6c47eb0844a2385fd1fc04168a4b700dcfbf3
Dan Graham <dan@saucelabs.com>
2023-06-20 22:46:09 +0000

    add Appium tests

---
+++ gitpod/test/specs/appium/appium.e2e.js
@@ -0,0 +1,8 @@
+// see ./wdio.saucelabs.conf.ts for configuration
+
+describe('Gitpod Appium Test', () => {
+    it('should visit a page and get the title', async () => {
+        // Add commands and assertions here
+    })
+})
+
