05c8e0deeda0ad809e300ae6f038e18851f4833a
Diego Molina <diemol@gmail.com>
2026-03-05 22:01:18 +0100

    Add setupNodeEvents to configure Edge browser launch options

--- saucectl/cypress-js/cypress.config.js
+++ saucectl/cypress-js/cypress.config.js
@@ -3,5 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://www.saucedemo.com',
+        setupNodeEvents(on, config) {
+            on('before:browser:launch', (browser, launchOptions) => {
+                if (browser.family === 'chromium' && browser.name === 'edge') {
+                    launchOptions.args.push('--no-sandbox')
+                }
+                return launchOptions
+            })
+        },
     },
 })
